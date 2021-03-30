afda7369e7765d79cce2b81c853f318bd26c2841
Adriaan Erasmus <adriaan.erasmus.za@gmail.com>
2021-03-30 22:02:55 +0200

    Fixed comms between nginx and php

--- compose/local/wordpress/entrypoint.sh
+++ compose/local/wordpress/entrypoint.sh
@@ -9,7 +9,7 @@ cd /var/www/html && composer update -vv
 echo "Setting permissions in /var/www/html/app and /var/www/html/wp"
 chown -R 1000:1000 /var/www/html/web/app
 chown -R 1000:1000 /var/www/html/web/wp
-cd /var/www/html/web && wp --allow-root s3-uploads enable
+#cd /var/www/html/web && wp --allow-root s3-uploads enable
 
 cmd="$@"
 exec $cmd
